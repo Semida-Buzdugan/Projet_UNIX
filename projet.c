d518db51236c7d5fba926724b6a5b5b6b94bd27d
Semida-Buzdugan <semida.buzdugan@dauphine.eu>
2020-06-17 16:47:46 +0200

    includes

---
+++ projet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main (){
+
+
+
+	return 0;
+}
