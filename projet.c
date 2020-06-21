d3ae193fa0022337da580488c8f6c0d82f964656
Sarah-Elhelw <sarah_elhelw@hotmail.fr>
2020-06-21 20:14:55 +0200

    Creating the pipes with a non blocking mode

--- projet.c
+++ projet.c
@@ -43,28 +43,64 @@ Fruit orange = {2, 5, 8};
 Fruit banana = {3, 6, 9};
 
 
-/* Function checking that the scenario is correct */
+/* Checking that the scenario is correct */
 
 void checkScenario(){
 	if (apple.quantity <0 || orange.quantity <0 || banana.quantity <0){
-		printf("Error : you entered a negative quantity");
+		printf("Error : you entered a negative quantity\n");
 		exit(1);
 	}
 	if (apple.quantity > apple.stock*10 || orange.quantity > orange.stock*10 || banana.quantity > banana.stock*20){
-		printf("Error : you entered a quantity superior to the stocks");
+		printf("Error : you entered a quantity superior to the stocks\n");
 		exit(1);
 	}
 	if (apple.price <0 || orange.price <0 || banana.price <0){
-		printf("Error : you entered a negative price");
+		printf("Error : you entered a negative price\n");
 		exit(1);
 	}
 }
+
+/* Creating a pipe */
+
+void pipeSucceed(int p[2]){
+	if (pipe(p) < 0){
+		printf("Program. Pipe is not supported.\n");
+		exit(1);
+	}
+}
+
+/* Activating the non blocking mode */
+
+void nonBlocking(int p[2]){
+	if (fcntl(p[0], F_SETFL, O_NONBLOCK) < 0){
+		printf("Program. Non blocking mode unavailable");
+		exit(2);
+	}
+} 
 	
 
 int main (){
 	checkScenario();
 	
-	printf("Helllo world!");
+	int p1[2], p2[2], p3[2], p4[2], p5[2], p6[2];
+
+	printf("Beginning of the scenario.\n");
+	
+	printf("Program. Creating the pipes.\n");
+	pipeSucceed(p1);
+	pipeSucceed(p2);
+	pipeSucceed(p3);
+	pipeSucceed(p4);
+	pipeSucceed(p5);
+	pipeSucceed(p6);
+	
+	printf("Program. Activating the non blocking mode for each pipe.\n");
+	nonBlocking(p1);
+	nonBlocking(p2);
+	nonBlocking(p3);
+	nonBlocking(p4);
+	nonBlocking(p5);
+	nonBlocking(p6);
 
 	return 0;
 }
